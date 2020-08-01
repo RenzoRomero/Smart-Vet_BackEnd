a0b9b1660be008320381a0311b0404b88864be20
Ruben <ruben2218@gmail.com>
2020-07-31 22:17:16 -0500

    Loguear Cliente

--- routes/index.js
+++ routes/index.js
@@ -1,6 +1,7 @@
 'use strict'
 
 const express = require('express')
+const ownerCtrl = require('../controllers/owner')
 const petCtrl = require('../controllers/pet')
 const api = express.Router()
 
@@ -9,5 +10,12 @@ api.get('/pet/:petId', petCtrl.getPet)
 api.post('/pet', petCtrl.savePet)
 api.put('/pet/:petId', petCtrl.updatePet)
 api.delete('/pet/:petId', petCtrl.deletePet)
+
+api.post('/owner/signup', ownerCtrl.signUp)
+api.post('/owner/signin', ownerCtrl.signIn)
+api.get('/owner', ownerCtrl.getOwners)
+api.get('/owner/:ownerId', ownerCtrl.getOwner)
+api.put('/owner/:ownerId', ownerCtrl.updateOwner)
+api.delete('/owner/:ownerId', ownerCtrl.deleteOwner)
 
 module.exports = api
