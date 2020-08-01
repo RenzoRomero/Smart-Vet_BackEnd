19076782d63e6ef87c717e62bed0004c202fd692
ManuelAponte <manuelaponte266@gmail.com>
2020-07-31 23:58:15 -0500

    Loguear Veterinario

--- routes/index.js
+++ routes/index.js
@@ -1,11 +1,18 @@
 'use strict'
 
 const express = require('express')
+const vetCtrl = require('../controllers/vet')
 const ownerCtrl = require('../controllers/owner')
 const petCtrl = require('../controllers/pet')
 const clinicHistoryCtrl = require('../controllers/clinicHistory')
 const auth = require('../middlewares/auth')
 const api = express.Router()
+
+api.post('/vet/signup', vetCtrl.signUp)
+api.post('/vet/signin', vetCtrl.signIn)
+api.get('/vet', vetCtrl.getVets)
+api.put('/vet/:vetId', vetCtrl.updateVet)
+api.delete('/vet/:vetId', vetCtrl.deleteVet)
 
 api.get('/pet', petCtrl.getPets)
 api.get('/pet/:petId', petCtrl.getPet)
