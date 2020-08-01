fda68f8ebed4a829d19b2f68c395adb05fbaef24
Victor <thevictor23@gmail.com>
2020-07-31 22:42:24 -0400

    Registrar Cliente

--- routes/index.js
+++ routes/index.js
@@ -1,6 +1,7 @@
 'use strict'
 
 const express = require('express')
+const ownerCtrl = require('../controllers/owner')
 const petCtrl = require('../controllers/pet')
 const api = express.Router()
 
@@ -9,5 +10,10 @@ api.get('/pet/:petId', petCtrl.getPet)
 api.post('/pet', petCtrl.savePet)
 api.put('/pet/:petId', petCtrl.updatePet)
 api.delete('/pet/:petId', petCtrl.deletePet)
+
+api.get('/owner', ownerCtrl.getOwners)
+api.get('/owner/:ownerId', ownerCtrl.getOwner)
+api.put('/owner/:ownerId', ownerCtrl.updateOwner)
+api.delete('/owner/:ownerId', ownerCtrl.deleteOwner)
 
 module.exports = api
